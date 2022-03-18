f6e10a80a506b762ecfb0c1198d89e1292153a24
Ester The Otter <83263521+EsterTheOtter@users.noreply.github.com>
2022-03-18 05:29:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Working with Loops
+
+// The for Loop
+  /*
+for ()
+let xVar= (1)
+let yVar= (xVar + 1)
+let zVar= (yVar + 1)
+
+for (i = 0; i < 10; i++) {
+  zVariable
+}
+console.log()
+/*
+
+let i = 0;
+while (i < 10) {
+  placeTile('mint')
+  placeTile('orange')
+  placeTile('mint')
+  i++
+}
+*/
+
+// The while Loop
+/* Idea: Abacus?
+Initial condition: all beads start away from the center beam
+10+2 columns, 2 beads top deck, 5 bottom deck
+As count up, to upper limit of 5, lower deck beads rise towards beam.
+On count of 5, lower deck column full
+If x > 5 on that column, lower deck beads drop back away from beam and single top
+deck bead drops to beam,
+
+*/
